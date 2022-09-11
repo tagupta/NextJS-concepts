5645cec2830787b93e23b1385467925e22304b14
tagupta <guptatanukhandelwal.96@gmail.com>
2022-09-11 14:00:32 +0530

    feat: prefetching+prerendering

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,6 +1,28 @@
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextjs-course-41eb5-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json"
+  );
+  const data = await response.json();
+  const transformedData = [];
+  for (const key in data) {
+    transformedData.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  return {
+    props: {
+      sales: transformedData,
+    },
+    // revalidate: 10,
+  };
+}
+
 const fetcher = (url) =>
   fetch(url)
     .then((response) => response.json())
@@ -16,60 +38,31 @@ const fetcher = (url) =>
       return transformedData;
     });
 
-const LastSalesPage = () => {
-  //   const [saleData, setSaleData] = useState();
-  //   const [isLoading, setIsLoading] = useState(true);
+const LastSalesPage = (props) => {
+  const [saleData, setsaleData] = useState(props.sales);
 
   const { data, error } = useSWR(
     "https://nextjs-course-41eb5-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json",
     fetcher
   );
 
-  //   useEffect(() => {
-  //     if (data) {
-  //       const transformedData = [];
-  //       for (const key in data) {
-  //         transformedData.push({
-  //           id: key,
-  //           userName: data[key].username,
-  //           volume: data[key].volume,
-  //         });
-  //       }
-  //       setSaleData(transformedData);
-  //     }
-  //   }, [data]);
-
-  //   useEffect(() => {
-  //     setIsLoading(true);
-  //     fetch(
-  //       "https://nextjs-course-41eb5-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json"
-  //     )
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         const transformedData = [];
-  //         for (const key in data) {
-  //           transformedData.push({
-  //             id: key,
-  //             userName: data[key].username,
-  //             volume: data[key].volume,
-  //           });
-  //         }
-  //         setSaleData(transformedData);
-  //         setIsLoading(false);
-  //       });
-  //   }, []);
+  useEffect(() => {
+    if (data) {
+      setsaleData(data);
+    }
+  }, [data]);
 
   if (error) {
     return <p>Failed to load.</p>;
   }
 
-  if (!data) {
+  if (!data && !saleData) {
     return <p>Loading...</p>;
   }
 
   return (
     <ul>
-      {data.map((sale) => (
+      {saleData.map((sale) => (
         <li key={sale.id}>
           {sale.userName} - ${sale.volume}
         </li>
